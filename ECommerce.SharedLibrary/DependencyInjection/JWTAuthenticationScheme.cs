8cd845105afd12ca640850aef2fe36bdee77fe06
Chetan Soni <chetansoni8101@gmail.com>
2025-11-19 23:35:50 +0530

    added Shared Libraries

---
+++ ECommerce.SharedLibrary/DependencyInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace ECommerce.SharedLibrary.DependencyInjection
+{
+    public static class JWTAuthenticationScheme
+    {
+        public static IServiceCollection AddJWTAuthenticationScheme(this IServiceCollection services, IConfiguration config) 
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:Key").Value!);
+                    string issuer = config.GetSection("Authentication:Issuer").Value!;
+                    string audience = config.GetSection("Authentication:Audience").Value!;
+
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key)
+                    };
+                });
+
+            return services;
+        }
+    }
+}
---
+++ ECommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace ECommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>
+            (this IServiceCollection services, IConfiguration config, string filename) where TContext : DbContext
+        {
+            // Add Generic Database Context
+            services.AddDbContext<TContext>(option => option.UseSqlServer(
+                config.GetConnectionString("eCommerceConnection"),
+                sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()));
+
+            //configure SeriLog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{filename}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            //JWT Authentication Scheme
+
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharedServices(this IApplicationBuilder app)
+        {
+            // Use Global Exception Middleware
+            app.UseMiddleware<Middleware.GlobalException>();
+
+            // Use Listen To Only Api Gateway Middleware
+            app.UseMiddleware<Middleware.ListenToOnlyAPIGateway>();
+            return app;
+        }
+    }
+}
