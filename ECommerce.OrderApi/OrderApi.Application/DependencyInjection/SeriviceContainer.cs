737d50dce7384adeb02d912b0da7bb8386288c6d
Chetan Soni <chetansoni8101@gmail.com>
2025-12-01 23:29:13 +0530

    completed Application layer and infrastructure layes for Order Api

---
+++ ECommerce.OrderApi/OrderApi.Application/DependencyInjection/SeriviceContainer.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.SharedLibrary.Logs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Services;
+using Polly;
+using Polly.Retry;
+
+namespace OrderApi.Application.DependencyInjection
+{
+    public static class SeriviceContainer
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+        {
+            // Register HttpClient Service
+            // Create Dependency Injection
+            services.AddHttpClient<IOrderService, OrderService>(options =>
+            {
+                options.BaseAddress = new Uri(config["ApiGateway:BaseAddress"]!);
+            });
+
+            // Create Retry Strategy
+            var retryStrategy = new RetryStrategyOptions()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<TaskCanceledException>(),
+                BackoffType = DelayBackoffType.Constant,
+                UseJitter = true,
+                OnRetry = args =>
+                {
+                    string message = $"OnRetry, Attempt: {args.AttemptNumber} Outcome {args.Outcome}";
+                    LogException.LogToConsole(message);
+                    LogException.LogToDebugger(message);
+                    return ValueTask.CompletedTask;
+                }
+            };
+
+            services.AddResiliencePipeline("my-resilience-pipeline", builder =>
+            {
+                builder.AddRetry(retryStrategy);
+            });
+
+            return services;
+        }
+    }
+}
---
+++ ECommerce.OrderApi/OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,41 @@
+﻿using ECommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            // Add Database Connectivity
+            // Add Authentication Scheme
+            SharedServiceContainer.AddSharedServices<OrderDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            // Create Dependency Injection
+            services.AddScoped<IOrder, OrderRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            // Register Middleware such as
+            // Global Exception -> handle external errors
+            // ListenToApiGateway -> block all outsider calls
+
+            SharedServiceContainer.UseSharedServices(app);
+            return app;
+        }
+    }
+}
